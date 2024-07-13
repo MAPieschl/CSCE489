21a7cf69938879711c32eb693b93c8f82a5fbb5d
Mike Pieschl <mapieschl@gmail.com>
2024-07-13 14:58:36 -0400

    Error checks added

--- CSCE489_Project1/shellfuncts.c
+++ CSCE489_Project1/shellfuncts.c
@@ -23,14 +23,39 @@
  *************************************************************************************/
 
 void select_command(int cmd_num, char cmd[MAX_NUM_ARGS][MAX_SIZE_ARGS]){
+	int error_check = 0;
 	switch(cmd_num){
 		case 0:
+			error_check = file_name_check(cmd[1]);
+			if(error_check != 0){exit(1);}
+			
+			error_check = file_exists_check(cmd[1]);
+			if(error_check != 0){
+				printf("\nFile already exists. Please choose a different name.");
+				exit(1);
+			}
+			
 			create(cmd[1]);
 			break;
 		case 1:
-			update(cmd[1], cmd[2], cmd[3]);
+			error_check = file_exists_check(cmd[1]);
+			if(error_check != 1){
+				printf("\nFile does not exist. Please use - create <file name> - to create a file prior to attempting to write to the file.");
+				exit(1);
+			}
+			
+			error_check = number_format_check(cmd[2]);
+			if(error_check < 0){exit(1);}
+			
+			update(cmd[1], error_check, cmd[3]);
 			break;
 		case 2:
+			error_check = file_exists_check(cmd[1]);
+			if(error_check != 1){
+				printf("\nFile does not exist. Please use - create <file name> - to create a file prior to attempting to write to the file.");
+				exit(1);
+			}
+		
 			list(cmd[1]);
 			break;
 		case 3:
@@ -43,50 +68,45 @@ void select_command(int cmd_num, char cmd[MAX_NUM_ARGS][MAX_SIZE_ARGS]){
 }
 
 /*************************************************************************************
- * hello - sends hello world to the user! I'm doing proper function commenting so future
- *         coders might not find my code as painful.
- *
- *    Params:  param2 - not a very good parameter name - something better might be
- *                      say, msgstr or sendtext
- *
+ * ERROR CHECKING FUNCTIONS
  *************************************************************************************/
 
-void create(char name[MAX_SIZE_ARGS]){
-	FILE *new_file;
-	new_file = fopen(name, "r");
+int file_name_check(char name[MAX_SIZE_ARGS){
+	for(int i = 0; i < MAX_SIZE_ARGS; i++){
+		if(cmd[1][i] == '\0'){
+			break;
+		}
+		else if(cmd[1][i] == '/' || cmd[1][i] == 0){
+			printf("\nCould not create file - illegal symbols present in ")
+			return 1;
+		}
+	}
 	
-	if(new_file == NULL){
-		new_file = fopen(name, "w");
-		fclose(new_file);
-		exit(0);
-	}
-	else{
-		printf("\nFile already exists!");
-		fclose(new_file);
-		exit(1);
-	}
+	return 0;
 }
 
-/*************************************************************************************
- * hello - sends hello world to the user! I'm doing proper function commenting so future
- *         coders might not find my code as painful.
- *
- *    Params:  param2 - not a very good parameter name - something better might be
- *                      say, msgstr or sendtext
- *
- *************************************************************************************/
+int file_exists_check(char name[MAX_SIZE_ARGS]){
+	int file_exists;
 
-void update(char name[MAX_SIZE_ARGS], char number[MAX_SIZE_ARGS], char text[MAX_SIZE_ARGS]){
+	FILE *check_file;
+	check_file = fopen(name, "r");
+	
+	if(check_file != NULL){
+		file_exists = 1;
+	}
+	else{
+		file_exists = 0;
+	}
+	
+	fclose(check_file);
+	
+	return file_exists;
+}
 
+int number_format_check(char number[MAX_SIZE_ARGS]){
 	int num_max_index;
 	int temp_val;
-	int num_iterations = 0;
-	char temp_string[MAX_SIZE_ARGS];
-	int temp_string_index = 0;
-	int inside_quotes = 0;
-
-	FILE *mod_file;
-	mod_file = fopen(name, "a");
+	int num_out;
 	
 	for (int i = 0; i < MAX_SIZE_ARGS; i++){
 		if(number[i] != '\0'){
@@ -98,9 +118,62 @@ void update(char name[MAX_SIZE_ARGS], char number[MAX_SIZE_ARGS], char text[MAX_
 	}
 	
 	for (int i = 0; i <= num_max_index; i++){
-		temp_val = (number[i] - 0x30)*pow(10.0, (double) num_max_index - i);
-		num_iterations += temp_val;
+		temp_val = number[i] - 0x30
+		
+		if((temp_val >= 0) && (temp_val <= 9)){
+			temp_val *= pow(10.0, (double) num_max_index - i);
+			num_out += temp_val;
+		}
+		else{
+			printf("\nUnable to proceed. Please ensure the number of iterations (argument 2) is a positive integer value.")
+			return -1;
+		}
 	}
+	
+	return num_out;
+}
+
+
+/*************************************************************************************
+ * TERMINAL FUNCTIONS
+ *************************************************************************************/
+
+
+/*************************************************************************************
+ * hello - sends hello world to the user! I'm doing proper function commenting so future
+ *         coders might not find my code as painful.
+ *
+ *    Params:  param2 - not a very good parameter name - something better might be
+ *                      say, msgstr or sendtext
+ *
+ *************************************************************************************/
+
+void create(char name[MAX_SIZE_ARGS]){
+	FILE *new_file;
+	new_file = fopen(name, "w");
+	fclose(new_file);
+	
+	exit(0);
+
+}
+
+/*************************************************************************************
+ * hello - sends hello world to the user! I'm doing proper function commenting so future
+ *         coders might not find my code as painful.
+ *
+ *    Params:  param2 - not a very good parameter name - something better might be
+ *                      say, msgstr or sendtext
+ *
+ *************************************************************************************/
+
+void update(char name[MAX_SIZE_ARGS], int number, char text[MAX_SIZE_ARGS]){
+
+	char temp_string[MAX_SIZE_ARGS];
+	int temp_string_index = 0;
+	int inside_quotes = 0;
+
+	FILE *mod_file;
+	mod_file = fopen(name, "a");
 	
 	for (int i = 0; i < MAX_SIZE_ARGS; i++){
 		if(text[i] == '"' && inside_quotes != 0){
@@ -112,7 +185,7 @@ void update(char name[MAX_SIZE_ARGS], char number[MAX_SIZE_ARGS], char text[MAX_
 		}
 	}
 	
-	for (int i = 0; i < num_iterations; i++){
+	for (int i = 0; i < number; i++){
 		fprintf(mod_file, "%s\n", temp_string);
 	}
 	
@@ -133,19 +206,6 @@ void update(char name[MAX_SIZE_ARGS], char number[MAX_SIZE_ARGS], char text[MAX_
  *************************************************************************************/
 
 void list(char name[MAX_SIZE_ARGS]){
-
-	// Ensure file exists
-	FILE *read_file;
-	read_file = fopen(name, "r");
-	
-	// If file doesn't exist, warn user and kill process with error
-	if(read_file == NULL){
-		printf("\nFile does not exist. Execute - create <file name> - to create a file.\n");
-		exit(1);
-	}
-	
-	fclose(read_file);
-	
 	// Run cat commadn to list file
 	printf("\n\n");
 	execl("/bin/cat", "cat %s", name, NULL);
